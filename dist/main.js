978cbac6bc1a37be3eca1daaf9355c85038e4566
jortega2 <juvenal.a.ortega@gmail.com>
2023-01-11 11:52:51 -0800

    added npm/webpack packages and configuration files

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('this song is ill nasty!');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudHBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiYWxlcnQoJ3RoaXMgc29uZyBpcyBpbGwgbmFzdHkhJyk7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/index.js
@@ -0,0 +1,1 @@
+alert('this song is ill nasty!');
