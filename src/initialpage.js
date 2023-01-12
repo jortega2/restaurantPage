9980cbdabe0d7e6e098d54a35c37c67cc71b878e
jortega2 <juvenal.a.ortega@gmail.com>
2023-01-11 19:16:27 -0800

    added information section to initial site

--- src/initialpage.js
+++ src/initialpage.js
@@ -40,7 +40,44 @@ function createMap() {
   return map;
 }
 
+function createHoursTable() {
+  const element = document.createElement('table');
+
+  const caption = document.createElement('caption');
+  caption.textContent = 'OPENING HOURS';
+  element.appendChild(caption);
+  const days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+
+  for (let i = 0; i < 7; i += 1) {
+    const day = document.createElement('tr');
+
+    const label = document.createElement('th');
+    label.innerText = days[i];
+
+    const hours = document.createElement('td');
+    hours.innerText = '8:00 a.m. - 10:00 p.m.';
+
+    day.append(label);
+    day.append(hours);
+    element.append(day);
+  }
+  return element;
+}
+
+function createInformation() {
+  const information = document.createElement('div');
+  information.classList.add('information');
+  information.classList.add('glow');
+
+  const hoursTable = createHoursTable();
+
+  information.append(hoursTable);
+
+  return information;
+}
+
 export default function initialPage(element) {
   element.append(createHeader());
   element.append(createMap());
+  element.append(createInformation());
 }
