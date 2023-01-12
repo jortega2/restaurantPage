6ecdf752e9c85ad15608392364d8f4f6ca689375
jortega2 <juvenal.a.ortega@gmail.com>
2023-01-11 17:15:03 -0800

    finished header section for the initial page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import { init } from 'es-module-lexer';
+import initialPage from './initialpage';
+
 const content = document.querySelector('#content');
+
+initialPage(content);
--- src/initialpage.js
+++ src/initialpage.js
@@ -1,3 +1,35 @@
-export default function initialPage() {
-  
+import './initialPage.css';
+import falloutLogo from './imgs/fallout.png';
+
+function createHeader() {
+  const header = document.createElement('div');
+  header.classList.add('header');
+  header.classList.add('glow');
+
+  const titleBG = document.createElement('div');
+  titleBG.classList.add('opaque');
+  titleBG.textContent = 'Power Noodles';
+
+  const nav = document.createElement('div');
+  nav.classList.add('navBar');
+
+  const fallout = document.createElement('img');
+  fallout.src = falloutLogo;
+  fallout.classList.add('titleImg');
+
+  const menu = document.createElement('button');
+  menu.classList.add('menu');
+  menu.textContent = 'menu';
+
+  nav.appendChild(fallout);
+  nav.appendChild(menu);
+
+  header.append(nav);
+  header.append(titleBG);
+
+  return header;
+}
+
+export default function initialPage(element) {
+  element.append(createHeader());
 }
