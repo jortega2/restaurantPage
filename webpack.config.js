e09e93e06952a55222260367283e498b2355e5bb
jortega2 <juvenal.a.ortega@gmail.com>
2023-01-11 19:23:13 -0800

    added raw-loader package to allow webpack to load txt files

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.txt$/i,
+        use: 'raw-loader',
+      },
     ],
   },
   devtool: 'inline-source-map',
