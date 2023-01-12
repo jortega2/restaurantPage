65af18d48c9578e0865eb09fa6d763b75f8c1a48
jortega2 <juvenal.a.ortega@gmail.com>
2023-01-12 12:50:15 -0800

    Implemented webPage in index.js to handle swapping from the home/menu pages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
 import { init } from 'es-module-lexer';
+import { set } from 'internal-slot';
 import initialPage from './initialpage';
 import createFooter from './footer';
 import createMenu from './menu';
 
-const content = document.querySelector('#content');
+const webPage = (() => {
+  const content = document.querySelector('#content');
+  let button;
 
-createMenu(content);
-createFooter(content);
+  const clear = function () {
+    content.innerText = '';
+  };
+
+  const setHomePage = function () {
+    clear();
+    initialPage(content);
+    createFooter(content);
+    buttonToggle(true);
+  };
+
+  const setMenuPage = function () {
+    clear();
+    createMenu(content);
+    createFooter(content);
+    buttonToggle(false);
+  };
+
+  const buttonToggle = function (fromHome) {
+    button = document.querySelector('.navButton');
+    if (fromHome) {
+      button.addEventListener('click', setMenuPage);
+    } else {
+      button.addEventListener('click', setHomePage);
+    }
+  };
+
+  // default
+  setHomePage();
+
+  button = document.querySelector('.navButton');
+  button.addEventListener('click', setMenuPage);
+})();
